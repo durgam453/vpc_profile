87979f433cdc33296aca502bd6b2997ad69e23fc
durgam453 <rajudurgam453@gmail.com>
2024-05-22 11:23:21 +0530

    Update vpc.tf
    update

--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,42 @@
+# Create VPC using Terraform Module
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "4.0.0"
+  version = "5.8.1"
 
-  name = var.vpc_name
-  cidr = var.vpc_cidr
 
-  azs             = var.availability_zones
-  private_subnets = var.private_subnets
+
+  name            = "${var.name}-${local.name}"
+  cidr            = var.cidr
+  azs             = var.azs
   public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  database_subnets                   = var.database_subnets
+  create_database_subnet_group       = var.create_database_subnet_group
+  create_database_subnet_route_table = var.create_database_subnet_route_table
 
-  tags = {
-    Terraform   = "true"
-    Environment = var.environment
+  # NAT Gateways - Outbound Communication
+  enable_nat_gateway = var.enable_nat_gateway
+  single_nat_gateway = var.single_nat_gateway
+
+  # DNS Parameters in VPC
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  # tags for the VPC
+  tags     = local.tags
+  vpc_tags = local.tags
+
+  public_subnet_tags = {
+    Name = "VPC Public Subnets"
   }
-}
+
+  private_subnet_tags = {
+    Name = "VPC Private Subnets"
+  }
+
+  database_subnet_tags = {
+    Name = "VPC Private Database Subnets"
+  }
+  # Instances launched into the Public subnet should be assigned a public IP address. Specify true to indicate that instances launched into the subnet should be assigned a public IP address
+  map_public_ip_on_launch = tr
